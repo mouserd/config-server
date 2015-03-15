6dcf765c8d5b089a8c4ca6249c4e852f316b900f
David Mouser <David.Mouser@afl.com.au>
2015-03-16 08:00:23 +1100

    Reverted previous packaging changes.

--- src/main/java/com/pixelus/configserver/ConfigServer.java
+++ src/main/java/com/pixelus/configserver/ConfigServer.java
@@ -2,21 +2,17 @@ package com.pixelus.configserver;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
-//@ImportResource({"classpath:aws-config.xml"})
-//@EnableContextInstanceData
-public class ConfigServer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(ConfigServer.class);
-    }
+@ImportResource({"classpath:aws-config.xml"})
+@EnableContextInstanceData
+public class ConfigServer {
 
     public static void main(final String[] args) {
         new SpringApplicationBuilder(ConfigServer.class)
