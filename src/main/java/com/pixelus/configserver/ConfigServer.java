0e78de173bba86ce0e3c1032c1f61c6158b1e68b
David Mouser <David.Mouser@afl.com.au>
2015-03-16 07:53:15 +1100

    Reconfigured as war application.

--- src/main/java/com/pixelus/configserver/ConfigServer.java
+++ src/main/java/com/pixelus/configserver/ConfigServer.java
@@ -2,13 +2,21 @@ package com.pixelus.configserver;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
-public class ConfigServer {
+//@ImportResource({"classpath:aws-config.xml"})
+//@EnableContextInstanceData
+public class ConfigServer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(ConfigServer.class);
+    }
 
     public static void main(final String[] args) {
         new SpringApplicationBuilder(ConfigServer.class)
